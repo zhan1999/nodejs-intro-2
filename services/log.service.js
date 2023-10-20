108cdae46a34bcaa94574f26551b7935d38c62df
Zhan <zhan1999@yahoo.com>
2023-10-20 17:17:35 +0200

    Add colored log to console

---
+++ services/log.service.js
@@ -0,0 +1,27 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+	console.log(chalk.bgRed(` ERROR + ${error}`));
+}
+const printSuccess = (message) => {
+	console.log(chalk.bgGreen(` SUCCEESS + ${message}`));
+}
+
+const printHelp = () => {
+	console.log(dedent(
+		`
+		${chalk.bgCyan(' HELP ')}
+		Without parameters - show weather
+		-s [CITY] - set city
+		-h - show help
+		-t [API_KEY] - save token
+		`
+	));
+
+}
+
+
+
+export {printError, printSuccess, printHelp};
+
--- weather.js
+++ weather.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js"
+import { printHelp } from './services/log.service.js';
 
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
-	console.log(args);
 	if (args.h) {
 		//help
+		printHelp();
+
 	}
 	if (args.s) {
 		// save city
